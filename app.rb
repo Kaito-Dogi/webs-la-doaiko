4dc9be182dd1a7cd4f65086d3b9c0df92fee8818
Kaito-Dogi <kendobu0405@gmail.com>
2021-09-20 16:42:37 +0000

    add: OAuth, Google Calendar APIのサンプルコードを追加

--- app.rb
+++ app.rb
@@ -3,18 +3,57 @@ Bundler.require
 require 'sinatra/reloader' if development?
 require 'sinatra/activerecord'
 require './models'
-require 'dotenv/load'
+require "google/apis/calendar_v3"
+require "googleauth"
+require "googleauth/stores/file_token_store"
+require 'google-id-token'
 
-enable :sessions
+LOGIN_URL = '/'
+
+configure do
+    Google::Apis::ClientOptions.default.application_name = 'DOAIKO'
+    Google::Apis::ClientOptions.default.application_version = '0.9'
+    Google::Apis::RequestOptions.default.retries = 3
+    
+    enable :sessions
+    
+    set :show_exceptions, false
+    set :client_id, Google::Auth::ClientId.new(ENV['GOOGLE_CLIENT_ID'],
+                                             ENV['GOOGLE_CLIENT_SECRET'])
+    set :token_store, Google::Auth::Stores::FileTokenStore.new(file: "token.yaml")
+end
+
+
+
+helpers do
+    # Returns credentials authorized for the requested scopes. If no credentials are available,
+    # redirects the user to authorize access.
+    def credentials_for(scope)
+        authorizer = Google::Auth::WebUserAuthorizer.new(settings.client_id, scope, settings.token_store)
+        user_id = session[:user_id]
+        redirect LOGIN_URL if user_id.nil?
+        credentials = authorizer.get_credentials(user_id, request)
+        if credentials.nil?
+            redirect authorizer.get_authorization_url(login_hint: user_id, request: request)
+        end
+        credentials
+    end
+    
+    def resize(url, width)
+        url.sub(/s220/, sprintf('s%d', width))
+    end
+end
 
 get '/' do
+    @client_id = settings.client_id.id
     puts "======================== #{session[:user_id]} ========================"
-    unless session[:user_id].nil?
-        @user = User.find(session[:user_id]) 
-        @user_courses = @user.user_courses
-        @user_classrooms = @user.user_classrooms
-    end
-    erb :index
+    puts "======================== #{session[:user_email]} ========================"
+    # unless session[:user_id].nil?
+    #     @user = User.find(session[:user_id]) 
+    #     @user_courses = @user.user_courses
+    #     @user_classrooms = @user.user_classrooms
+    # end
+    erb :home
 end
 
 get '/signup' do
