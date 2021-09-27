59e9f0a323b01af19da486c210a594de006b8a0b
Kaito-Dogi <kendobu0405@gmail.com>
2021-09-27 16:38:16 +0000

    add: mypageでユーザーの情報を登録

--- app.rb
+++ app.rb
@@ -93,67 +93,33 @@ post '/search' do
     redirect '/'
 end
 
-post '/signup' do
-    user = User.create!(
-        name: params[:name],
-        email: params[:email],
-        area_id: params[:area_id],
-        password: params[:password],
-        password_confirmation: params[:password_confirmation]
-    )
-    if user.persisted?
-        session[:token_key] = user.id
-        
-        course_ids = [
-            params[:iphone],
-            params[:android],
-            params[:unity],
-            params[:webd],
-            params[:webs]
-        ]
-        course_ids.each do |course_id|
-            unless course_id.nil?
-                UserCourse.create!(
-                    user_id: user.id,
-                    course_id: course_id,
-                    level: 1
-                )
-            end
-        end
-        
-        classroom_ids = [
-            params[:tue_weekly_shirokane],
-            params[:wed_weekly_shirokane],
-            params[:thu_weekly_shirokane],
-            params[:fri_weekly_shirokane],
-            params[:sat_weekly_shirokane],
-            params[:sat_b_shirokane],
-            params[:sun_a_shirokane],
-            params[:sun_b_shirokane],
-            params[:sun_b_akihabara],
-            params[:sun_b_ikebukuro],
-            params[:sat_a_yokohama],
-            params[:sun_a_yokohama],
-            params[:fri_weekly_osaka],
-            params[:sat_a_osaka],
-            params[:sun_a_osaka],
-            params[:sun_b_osaka],
-            params[:sat_a_nagoya],
-            params[:sun_a_nagoya],
-            params[:fri_weekly_online],
-            params[:sat_weekly_online],
-            params[:sun_b_online]
-        ]
-        classroom_ids.each do |classroom_id|
-            unless classroom_id.nil?
-                UserClassroom.create!(
-                    user_id: user.id,
-                    classroom_id: classroom_id
-                )
-            end
-        end
+post '/update' do
+    user = current_user
+
+    user.nickname = params[:nickname]
+    user.area_id = params[:area_id]
+    user.term = params[:term]
+
+    user.courses.destroy_all
+    course_ids = params[:course_ids]
+    course_ids.each do |course_id|
+        UserCourse.create!(
+            user_id: user.id,
+            course_id: course_id,
+            level: 1
+        )
     end
-    
+
+    user.classrooms.destroy_all
+    classroom_ids = params[:classroom_ids]
+    classroom_ids.each do |classroom_id|
+        UserClassroom.create!(
+            user_id: user.id,
+            classroom_id: classroom_id
+        )
+    end
+
+    user.save!
     redirect '/'
 end
 
